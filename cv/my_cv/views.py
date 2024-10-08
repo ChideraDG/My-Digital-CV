907719231659553306c056c6d4d7684ab9b22c7f
Chidera Divine-gift <chrischidera6@gmail.com>
2024-10-08 10:27:52 +0100

    Update views.py

--- cv/my_cv/views.py
+++ cv/my_cv/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.mail import send_mail
+from django.conf import settings
+from .forms import ContactForm
 
 def home(request):
     context = {
@@ -41,3 +45,27 @@ def calculator(request):
 
 def tic_tac_toe(request):
     return render(request, "portfolio_tic_detail.html")
+
+def contact_me(request):
+    form = ContactForm(request.POST if request.method == 'POST' else None)
+
+    if request.method == 'POST' and form.is_valid():
+        name = form.cleaned_data.get('name')
+        email = form.cleaned_data.get('email')
+        subject = form.cleaned_data.get('subject')
+        message = form.cleaned_data.get('message')
+
+        send_mail(from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=['chrischidera6@gmail.com', 'chidera.ohanenye-ohiaekpete@stu.cu.edu.ng'],
+                    subject=subject,
+                    message=f"Sender's Name: {name.title()} \n\nSender's Email: {email.lower()} \n\n" + message,
+                    fail_silently=False)
+
+        return JsonResponse({'success': True})
+    elif request.method == 'POST':
+        return JsonResponse({'success': False, 'error': 'Invalid form submission.'})
+
+    context = {
+        'form': form,
+    }
+    return render(request, "contact_me.html", context)
